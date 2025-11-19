1b73f04c5c918348352953c6785ba39beb473311
Lê Văn Đạt <levandat10a14@gmail.com>
2025-11-19 14:39:43 +0700

    Coming Soon

--- config/config.php
+++ config/config.php
@@ -4,8 +4,8 @@ return [
     'mysql' => [
         'host' => getenv('MYSQL_HOST') ?: 'sql101.infinityfree.com',
         'port' => (int) (getenv('MYSQL_PORT') ?: 3306),
-        'database' => getenv('MYSQL_DB') ?: 'ifo_40414526_db_bannhaccu',
-        'user' => getenv('MYSQL_USER') ?: 'ifo_40414526',
+        'database' => getenv('MYSQL_DB') ?: 'if0_40414526_db_bannhaccu',
+        'user' => getenv('MYSQL_USER') ?: 'if0_40414526',
         'pass' => getenv('MYSQL_PASS') ?: 'iemcbz3MSF',
         'charset' => 'utf8mb4',
     ],
