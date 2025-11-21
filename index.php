a373f0843c88651222d4e0f9b05bd3cd6aa7ecc9
Lê Văn Đạt <levandat10a14@gmail.com>
2025-11-21 14:21:52 +0700

    V

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 
     if(isset($isConnected) && $isConnected) {
         require __DIR__ . '/templates/home.php';
-        $sql = 'SELECT * FROM `nguoidung`';
+        $sql = 'SELECT * FROM `nguoi_dung`';
         $stmt = $pdo->query($sql);
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         foreach($result as $row) {
             echo "Mã: " . $row['ma_nd'] . " - Tên: " . $row['tennd'] . " - Email: " . $row['email'] . "<br/>";
         }
     } else {
-        echo "<br/>Kết nối MySQL thất bại từ index.php\n";
+        echo "<br/>Kết nối cơ sở dữ liệu thất bại!!!\n";
     }
 ?>
